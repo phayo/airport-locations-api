d960045f165f022a2e05e0915fad07709ebf51a9
phayo <anazodogodwin@gmail.com>
2019-12-03 19:57:21 +0100

    modify exception Handler

--- src/main/java/com/ebuka/locationsapi/exception/GlogalExceptionHandler.java
+++ src/main/java/com/ebuka/locationsapi/exception/GlogalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ebuka.locationsapi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 /**
  * GlogalExceptionHandler
